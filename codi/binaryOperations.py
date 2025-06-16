4dcc79bab22295edc50e5d5e46d38e1e9a00d1ff
fernando <fernando.recerca@gmail.com>
2025-06-16 10:59:57 +0200

    Afegeixo dos fitxers per començar a treballar en python

---
+++ codi/binaryOperations.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+	print(a, end="")
+	print(" + ", end="")
+	print(b, end="")
+	print(" = ", end="")
+	print(a+b)
+	return
+
+def producte (a, b):
+  return a*b
---
+++ codi/dummy.py
@@ -0,0 +1,6 @@
+import binaryOperations as bo
+
+bo.suma(3,5)
+
+print (bo.producte(-2,5))
+
